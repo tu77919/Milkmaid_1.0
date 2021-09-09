9a08ce0395a0f4be6dcde0fef76060da8db799a2
tu77919 <denny.r.heath@gmail.com>
2021-09-08 22:53:03 -0700

    Implemented Edit & Update methods and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,15 @@ class ArticlesController < ApplicationController
     #byebug
   	@article = Article.find(params[:id])                                                             #Take Article class and find passed in parameter ID and store to INSTANCE variable to be used outside of method
   end
-
   def index
     @articles = Article.all                                                                         #Take Article class, using all method and gather all articles in the table and pass to INSTANCE Variable to be used in index page
   end
-
   def new
     @article = Article.new()
   end
-
+  def edit
+    @article = Article.find(params[:id])                                                            #Use the Article.find() Class and method with the passed in Params hash to find ID and use in the url
+  end
   def create
     #very simple type of info pulling in debug sort of way.
     #render plain: params[:article]
@@ -27,6 +27,15 @@ class ArticlesController < ApplicationController
       redirect_to @article                                                                          #another way to redirect to article_path is to divine article_path from instance varialbe the method with give you the ID needed
     else
       render 'new'
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
