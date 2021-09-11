f7cbba826568b1fae95610fb6d57e0da1442ba15
tu77919 <denny.r.heath@gmail.com>
2021-09-10 22:19:33 -0700

    Refactored and cleaned up code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]                               #Ruby helper BEFORE_ACTION is a set point that says BEFORE DOING ACTIONS EXECUTE METHOD SET_ARTICLE - But if its a selective set of code - specify the actions to utilize the method call here SET_ARTICLE
+
   def show
     #byebug
-  	@article = Article.find(params[:id])                                                             #Take Article class and find passed in parameter ID and store to INSTANCE variable to be used outside of method
   end
   def index
     @articles = Article.all                                                                         #Take Article class, using all method and gather all articles in the table and pass to INSTANCE Variable to be used in index page
@@ -10,13 +11,12 @@ class ArticlesController < ApplicationController
     @article = Article.new()
   end
   def edit
-    @article = Article.find(params[:id])                                                            #Use the Article.find() Class and method with the passed in Params hash to find ID and use in the url
   end
   def create
     #very simple type of info pulling in debug sort of way.
     #render plain: params[:article]
 
-    @article = Article.new(params.require(:article).permit(:title, :description))                   #Create method called by post call from form submit button chain methods from passed in params object to obtain desired fields and set them up to written to db
+    @article = Article.new(article_params)                   #Create method called by post call from form submit button chain methods from passed in params object to obtain desired fields and set them up to written to db
     #checking content of form in real time upon submit - inspect object and obtain all fields
     #render plain: @article.inspect
 
@@ -30,8 +30,7 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -39,8 +38,17 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private                                                                                            #Keyword PRIVATE indicates private methods beneath so they are not universal to the program
+
+  def set_article                                                                                    #Method contains 1 line of code universal to 4 different actions SHOW, EDIT, UPDATE, DESTROY
+    @article = Article.find(params[:id])                                                             #Take Article class and find passed in parameter ID and store to INSTANCE variable to be used outside of method
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
